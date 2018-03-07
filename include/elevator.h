ee149a3fd5a0d26695704407bfb48f7db2d27ded
Jai Khanna <jai41khanna@gmail.com>
2018-03-07 04:15:25 +0100

    add initial files

---
+++ include/elevator.h
@@ -0,0 +1,12 @@
+/**
+ * @brief 
+ * 
+ * @file elevator.h
+ * @author Jai Khanna
+ * @date 2018-03-07
+ */
+
+#ifndef ELEVATOR
+#define ELEVATOR
+
+#endif
---
+++ src/simulate.cpp
@@ -0,0 +1,17 @@
+/**
+ * @brief 
+ * 
+ * @file simulate.cpp
+ * @author Jai Khanna
+ * @date 2018-03-07
+ */
+
+#include <iostream>
+#include "elevator.h"
+using namespace std;
+
+int main(int argc, char* argv[])
+{
+    Elevator myElevator;
+    return 0;
+}
